4d90b29d8632d6d4a1800975c0585067787fe0fd
SasiGanesan <sasiganesan2001@gmail.com>
2024-01-23 16:47:21 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB= async () => {
     try {
       const conn = await mongoose.connect(process.env.MONGO_URI,{
-        retryWrites:true,
+        retryWrites:false,
         serverSelectionTimeoutMS: 5000
       });
       
