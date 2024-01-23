d282ab07279f22539675c1cc87ee0b7b7927a6ee
SasiGanesan <sasiganesan2001@gmail.com>
2024-01-23 13:50:57 +0530

    committed

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,12 +6,13 @@ const connectDB= async () => {
         retryWrites:true,
         serverSelectionTimeoutMS:5000
       });
+      
       // console.log("hello",process.env.MONGO_URI);
       console.log(`MongoDB Connected: ${conn.connection.host}`);
-      // console.log("hi");
+   
     } catch (error) {
       console.log(`error: ${error.message}`);
-      // console.log("hii");
+
     }
   };
   export default connectDB
